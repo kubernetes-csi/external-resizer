eb1e1b4b9ff77b727500bea78a08126094be131e
mlmhl <malinbupt@gmail.com>
2018-12-25 14:51:13 +0800

    add initial skeleton of the resize controller

---
+++ cmd/csi-resizer/main.go
@@ -0,0 +1,85 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"flag"
+	"time"
+
+	"github.com/kubernetes-csi/external-resizer/pkg/controller"
+	"github.com/kubernetes-csi/external-resizer/pkg/resizer"
+	"github.com/kubernetes-csi/external-resizer/pkg/util"
+
+	"k8s.io/klog"
+)
+
+var (
+	master       = flag.String("master", "", "Master URL to build a client config from. Either this or kubeconfig needs to be set if the provisioner is being run out of cluster.")
+	kubeConfig   = flag.String("kubeconfig", "", "Absolute path to the kubeconfig")
+	resyncPeriod = flag.Duration("resync-period", time.Minute*10, "Resync period for cache")
+	workers      = flag.Int("workers", 10, "Concurrency to process multiple resize requests")
+
+	enableLeaderElection      = flag.Bool("leader-election", false, "Enable leader election.")
+	leaderElectionIdentity    = flag.String("leader-election-identity", "", "Unique identity of this resizer. Typically name of the pod where the resizer runs.")
+	leaderElectionNamespace   = flag.String("leader-election-namespace", "kube-system", "Namespace where this resizer runs.")
+	leaderElectionRetryPeriod = flag.Duration("leader-election-retry-period", time.Second*5,
+		"The duration the clients should wait between attempting acquisition and renewal "+
+			"of a leadership. This is only applicable if leader election is enabled.")
+	leaderElectionLeaseDuration = flag.Duration("leader-election-lease-duration", time.Second*15,
+		"The duration that non-leader candidates will wait after observing a leadership "+
+			"renewal until attempting to acquire leadership of a led but unrenewed leader "+
+			"slot. This is effectively the maximum duration that a leader can be stopped "+
+			"before it is replaced by another candidate. This is only applicable if leader "+
+			"election is enabled.")
+	leaderElectionRenewDeadLine = flag.Duration("leader-election-renew-deadline", time.Second*10,
+		"The duration that non-leader candidates will wait after observing a leadership "+
+			"renewal until attempting to acquire leadership of a led but unrenewed leader "+
+			"slot. This is effectively the maximum duration that a leader can be stopped "+
+			"before it is replaced by another candidate. This is only applicable if leader "+
+			"election is enabled.")
+)
+
+func main() {
+	klog.InitFlags(nil)
+	flag.Set("logtostderr", "true")
+	flag.Parse()
+
+	resizerName := "csi/example-resizer"
+
+	kubeClient, err := util.NewK8sClient(*master, *kubeConfig)
+	if err != nil {
+		klog.Fatal(err.Error())
+	}
+
+	var leaderElectionConfig *util.LeaderElectionConfig
+	if *enableLeaderElection {
+		if leaderElectionIdentity == nil || *leaderElectionIdentity == "" {
+			klog.Fatal("--leader-election-identity must not be empty")
+		}
+		leaderElectionConfig = &util.LeaderElectionConfig{
+			Identity:      *leaderElectionIdentity,
+			LockName:      util.SanitizeName(resizerName),
+			Namespace:     *leaderElectionNamespace,
+			RetryPeriod:   *leaderElectionRetryPeriod,
+			LeaseDuration: *leaderElectionLeaseDuration,
+			RenewDeadLine: *leaderElectionRenewDeadLine,
+		}
+	}
+
+	rc := controller.NewResizeController(resizerName, resizer.New(), kubeClient, *resyncPeriod)
+	rc.Run(*workers, leaderElectionConfig)
+}
---
+++ pkg/util/util.go
@@ -0,0 +1,162 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package util
+
+import (
+	"encoding/json"
+	"fmt"
+	"regexp"
+
+	"k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/strategicpatch"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+var knownResizeConditions = map[v1.PersistentVolumeClaimConditionType]bool{
+	v1.PersistentVolumeClaimResizing:                true,
+	v1.PersistentVolumeClaimFileSystemResizePending: true,
+}
+
+// NewK8sClient is an utility function used to create a kubernetes sdk client.
+func NewK8sClient(master, kubeConfig string) (kubernetes.Interface, error) {
+	var config *rest.Config
+	var err error
+	if master != "" || kubeConfig != "" {
+		config, err = clientcmd.BuildConfigFromFlags(master, kubeConfig)
+	} else {
+		config, err = rest.InClusterConfig()
+	}
+	if err != nil {
+		return nil, fmt.Errorf("failed to create config: %v", err)
+	}
+	kubeClient, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create client: %v", err)
+	}
+	return kubeClient, nil
+}
+
+// PVCKey returns an unique key of a PVC object,
+func PVCKey(pvc *v1.PersistentVolumeClaim) string {
+	return fmt.Sprintf("%s/%s", pvc.Namespace, pvc.Name)
+}
+
+// MergeResizeConditionsOfPVC updates pvc with requested resize conditions
+// leaving other conditions untouched.
+func MergeResizeConditionsOfPVC(oldConditions, newConditions []v1.PersistentVolumeClaimCondition) []v1.PersistentVolumeClaimCondition {
+	newConditionSet := make(map[v1.PersistentVolumeClaimConditionType]v1.PersistentVolumeClaimCondition, len(newConditions))
+	for _, condition := range newConditions {
+		newConditionSet[condition.Type] = condition
+	}
+
+	var resultConditions []v1.PersistentVolumeClaimCondition
+	for _, condition := range oldConditions {
+		// If Condition is of not resize type, we keep it.
+		if _, ok := knownResizeConditions[condition.Type]; !ok {
+			newConditions = append(newConditions, condition)
+			continue
+		}
+		if newCondition, ok := newConditionSet[condition.Type]; ok {
+			// Use the new condition to replace old condition with same type.
+			resultConditions = append(resultConditions, newCondition)
+			delete(newConditionSet, condition.Type)
+		}
+
+		// Drop old conditions whose type not exist in new conditions.
+	}
+
+	// Append remains resize conditions.
+	for _, condition := range newConditionSet {
+		resultConditions = append(resultConditions, condition)
+	}
+
+	return resultConditions
+}
+
+// PatchPVCStatus updates PVC status using PATCH verb
+func PatchPVCStatus(
+	oldPVC *v1.PersistentVolumeClaim,
+	newPVC *v1.PersistentVolumeClaim,
+	kubeClient kubernetes.Interface) (*v1.PersistentVolumeClaim, error) {
+	patchBytes, err := getPatchData(oldPVC, newPVC)
+	if err != nil {
+		return nil, fmt.Errorf("can't patch status of PVC %s as generate path data failed: %v", PVCKey(oldPVC), err)
+	}
+	updatedClaim, updateErr := kubeClient.CoreV1().PersistentVolumeClaims(oldPVC.Namespace).
+		Patch(oldPVC.Name, types.StrategicMergePatchType, patchBytes, "status")
+	if updateErr != nil {
+		return nil, fmt.Errorf("can't patch status of  PVC %s with %v", PVCKey(oldPVC), updateErr)
+	}
+	return updatedClaim, nil
+}
+
+// UpdatePVCapacity updates PVC capacity with requested size.
+func UpdatePVCapacity(pv *v1.PersistentVolume, newCapacity resource.Quantity, kubeClient kubernetes.Interface) error {
+	newPV := pv.DeepCopy()
+	newPV.Spec.Capacity[v1.ResourceStorage] = newCapacity
+	patchBytes, err := getPatchData(pv, newPV)
+	if err != nil {
+		return fmt.Errorf("can't update capacity of PV %s as generate path data failed: %v", pv.Name, err)
+	}
+	_, updateErr := kubeClient.CoreV1().PersistentVolumes().Patch(pv.Name, types.StrategicMergePatchType, patchBytes)
+	if updateErr != nil {
+		return fmt.Errorf("update capacity of PV %s failed: %v", pv.Name, updateErr)
+	}
+	return nil
+}
+
+func getPatchData(oldObj, newObj interface{}) ([]byte, error) {
+	oldData, err := json.Marshal(oldObj)
+	if err != nil {
+		return nil, fmt.Errorf("marshal old object failed: %v", err)
+	}
+	newData, err := json.Marshal(newObj)
+	if err != nil {
+		return nil, fmt.Errorf("mashal new object failed: %v", err)
+	}
+	patchBytes, err := strategicpatch.CreateTwoWayMergePatch(oldData, newData, oldObj)
+	if err != nil {
+		return nil, fmt.Errorf("CreateTwoWayMergePatch failed: %v", err)
+	}
+	return patchBytes, nil
+}
+
+// HasFileSystemResizePendingCondition returns true if a pvc has a FileSystemResizePending condition.
+// This means the controller side resize operation is finished, and kublete side operation is in progress.
+func HasFileSystemResizePendingCondition(pvc *v1.PersistentVolumeClaim) bool {
+	for _, condition := range pvc.Status.Conditions {
+		if condition.Type == v1.PersistentVolumeClaimFileSystemResizePending && condition.Status == v1.ConditionTrue {
+			return true
+		}
+	}
+	return false
+}
+
+// SanitizeName changes any name to a sanitized name which can be accepted by kubernetes.
+func SanitizeName(name string) string {
+	re := regexp.MustCompile("[^a-zA-Z0-9-]")
+	name = re.ReplaceAllString(name, "-")
+	if name[len(name)-1] == '-' {
+		// name must not end with '-'
+		name = name + "X"
+	}
+	return name
+}
