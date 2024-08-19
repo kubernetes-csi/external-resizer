kubernetes-csi/external-resizer

commit 99cf66fd7606604ac3dd101b6a8141d9ff9a1366

Add new slow queue
Fix code to handle slow retries for errors

pkg/util/slowset.go
@@ -1,0 +1,96 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package util
+
+import (
+	"sync"
+	"time"
+)
+
+const (
+	slowSetSleepDelay = 100 * time.Millisecond
+)
+
+// SlowSet is a set of API objects that should be synced at slower rate. Key is typically the object
+// namespace + name and value is timestamp when the object was added to the set.
+type SlowSet struct {
+	sync.RWMutex
+	// retentionTime is the time after which an item will be removed from the set
+	// this indicates, how long before an operation on pvc can be retried.
+	retentionTime time.Duration
+	workSet       map[string]time.Time
+}
+
+func NewSlowSet(retTime time.Duration) *SlowSet {
+	return &SlowSet{
+		retentionTime: retTime,
+		workSet:       make(map[string]time.Time),
+	}
+}
+
+func (s *SlowSet) Add(key string) bool {
+	s.Lock()
+	defer s.Unlock()
+
+	if _, ok := s.workSet[key]; ok {
+		return false
+	}
+
+	s.workSet[key] = time.Now()
+	return true
+}
+
+func (s *SlowSet) Contains(key string) bool {
+	s.RLock()
+	defer s.RUnlock()
+
+	_, ok := s.workSet[key]
+	return ok
+}
+
+func (s *SlowSet) Remove(key string) {
+	s.Lock()
+	defer s.Unlock()
+
+	delete(s.workSet, key)
+}
+
+func (s *SlowSet) TimeRemaining(key string) time.Duration {
+	s.RLock()
+	defer s.RUnlock()
+
+	if startTime, ok := s.workSet[key]; ok {
+		return s.retentionTime - time.Since(startTime)
+	}
+	return 0
+}
+
+func (s *SlowSet) Run(stopCh <-chan struct{}) {
+	for {
+		select {
+		case <-stopCh:
+			return
+		default:
+			time.Sleep(slowSetSleepDelay)
+			for key, t := range s.workSet {
+				if time.Since(t) > s.retentionTime {
+					s.Remove(key)
+				}
+			}
+		}
+	}
+}

cmd/csi-resizer/main.go
@@ -211,7 +211,8 @@ func main() {
 	resizerName := csiResizer.Name()
 	rc := controller.NewResizeController(resizerName, csiResizer, kubeClient, *resyncPeriod, informerFactory,
 		workqueue.NewItemExponentialFailureRateLimiter(*retryIntervalStart, *retryIntervalMax),
-		*handleVolumeInUseError)
+		*handleVolumeInUseError, *retryIntervalMax)
+
 	modifierName := csiModifier.Name()
 	var mc modifycontroller.ModifyController
 	// Add modify controller only if the feature gate is enabled

pkg/controller/controller.go
@@ -69,6 +69,10 @@ type resizeController struct {
 	podLister       corelisters.PodLister
 	podListerSynced cache.InformerSynced
 
+	// slowSet is used to track PVCs for which expansion failed with infeasible error
+	// and should be retried at slower rate.
+	slowSet *util.SlowSet
+
 	// a cache to store PersistentVolume objects
 	volumes cache.Store
 	// a cache to store PersistentVolumeClaim objects
@@ -84,7 +88,8 @@ func NewResizeController(
 	resyncPeriod time.Duration,
 	informerFactory informers.SharedInformerFactory,
 	pvcRateLimiter workqueue.RateLimiter,
-	handleVolumeInUseError bool) ResizeController {
+	handleVolumeInUseError bool,
+	maxRetryInterval time.Duration) ResizeController {
 	pvInformer := informerFactory.Core().V1().PersistentVolumes()
 	pvcInformer := informerFactory.Core().V1().PersistentVolumeClaims()
 	eventBroadcaster := record.NewBroadcaster()
@@ -106,6 +111,7 @@ func NewResizeController(
 		volumes:                pvInformer.Informer().GetStore(),
 		claims:                 pvcInformer.Informer().GetStore(),
 		eventRecorder:          eventRecorder,
+		slowSet:                util.NewSlowSet(maxRetryInterval),
 		finalErrorPVCs:         sets.New[string](),
 		usedPVCs:               newUsedPVCStore(),
 		handleVolumeInUseError: handleVolumeInUseError,
@@ -234,6 +240,8 @@ func (ctrl *resizeController) updatePVC(oldObj, newObj interface{}) {
 		//    unrelated to volume resize.
 		// 2. Informer resynced the PVC and send this Update event without any changes.
 		// 3. PV's filesystem has recently been resized and requires removal of its resize annotation
+		// 4. Though the size is same, the resize status has changed. This can happen when the resize operation failed
+		//    on the node.
 		//
 		// If it is case 1, we can just discard this event. If case 2 or 3, we need to put it into the queue to
 		// perform a resync operation.
@@ -275,6 +283,10 @@ func (ctrl *resizeController) Run(workers int, ctx context.Context) {
 		return
 	}
 
+	if utilfeature.DefaultFeatureGate.Enabled(features.RecoverVolumeExpansionFailure) {
+		go ctrl.slowSet.Run(stopCh)
+	}
+
 	for i := 0; i < workers; i++ {
 		go wait.Until(ctrl.syncPVCs, 0, stopCh)
 	}
@@ -290,10 +302,18 @@ func (ctrl *resizeController) syncPVCs() {
 	}
 	defer ctrl.claimQueue.Done(key)
 
-	if err := ctrl.syncPVC(key.(string)); err != nil {
-		// Put PVC back to the queue so that we can retry later.
-		klog.ErrorS(err, "Error syncing PVC")
-		ctrl.claimQueue.AddRateLimited(key)
+	err := ctrl.syncPVC(key.(string))
+
+	if err != nil {
+		if utilfeature.DefaultFeatureGate.Enabled(features.RecoverVolumeExpansionFailure) && util.IsDelayRetryError(err) {
+			// If the error is a DelayRetryError, we should requeue the PVC with a delay.
+			delayRetryError := err.(*util.DelayRetryError)
+			ctrl.claimQueue.AddAfter(key, delayRetryError.TryAfter())
+		} else {
+			// Put PVC back to the queue so that we can retry later.
+			klog.ErrorS(err, "Error syncing PVC")
+			ctrl.claimQueue.AddRateLimited(key)
+		}
 	} else {
 		ctrl.claimQueue.Forget(key)
 	}

pkg/controller/controller_test.go
@@ -243,7 +243,11 @@ func TestController(t *testing.T) {
 		}
 
 		featuregatetesting.SetFeatureGateDuringTest(t, utilfeature.DefaultFeatureGate, features.AnnotateFsResize, true)
-		controller := NewResizeController(driverName, csiResizer, kubeClient, time.Second, informerFactory, workqueue.DefaultControllerRateLimiter(), !test.disableVolumeInUseErrorHandler)
+		controller := NewResizeController(driverName, csiResizer,
+			kubeClient, time.Second,
+			informerFactory, workqueue.DefaultControllerRateLimiter(),
+			!test.disableVolumeInUseErrorHandler,
+			2*time.Minute /* maxRetryInterval */)
 
 		ctrlInstance, _ := controller.(*resizeController)
 
@@ -404,7 +408,11 @@ func TestResizePVC(t *testing.T) {
 			}
 
 			featuregatetesting.SetFeatureGateDuringTest(t, utilfeature.DefaultFeatureGate, features.AnnotateFsResize, true)
-			controller := NewResizeController(driverName, csiResizer, kubeClient, time.Second, informerFactory, workqueue.DefaultControllerRateLimiter(), true /* disableVolumeInUseErrorHandler*/)
+			controller := NewResizeController(driverName, csiResizer,
+				kubeClient, time.Second,
+				informerFactory, workqueue.DefaultControllerRateLimiter(),
+				true, /* disableVolumeInUseErrorHandler*/
+				2*time.Minute /* maxRetryInterval */)
 
 			ctrlInstance, _ := controller.(*resizeController)
 

pkg/controller/expand_and_recover.go
@@ -50,6 +50,8 @@ func (ctrl *resizeController) expandAndRecover(pvc *v1.PersistentVolumeClaim, pv
 		return pvc, pv, fmt.Errorf("error getting object key for pvc %s: %v", pvc.Name, err), resizeNotCalled
 	}
 
+	inSlowSet := ctrl.slowSet.Contains(pvcKey)
+
 	var allocatedSize *resource.Quantity
 	t, ok := pvc.Status.AllocatedResources[v1.ResourceStorage]
 	if ok {
@@ -57,6 +59,7 @@ func (ctrl *resizeController) expandAndRecover(pvc *v1.PersistentVolumeClaim, pv
 	}
 
 	updateStatus := true
+	ctrl.markForSlowRetry(pvcKey, resizeStatus)
 
 	if pvSize.Cmp(pvcSpecSize) < 0 {
 		// PV is smaller than user requested size. In general some control-plane volume expansion
@@ -140,6 +143,12 @@ func (ctrl *resizeController) expandAndRecover(pvc *v1.PersistentVolumeClaim, pv
 	// If we are expanding volume to same size as before, then there is no point in changing
 	// status fields again.
 	if allocatedSize != nil && allocatedSize.Cmp(newSize) == 0 {
+		if inSlowSet {
+			msg := fmt.Sprintf("skipping volume expansion for pvc %s, because expansion previously failed with infeasible error", pvcKey)
+			klog.V(4).Infof(msg)
+			delayRetryError := util.NewDelayRetryError(msg, ctrl.slowSet.TimeRemaining(pvcKey))
+			return pvc, pv, delayRetryError, resizeNotCalled
+		}
 		klog.V(4).Infof("skipping updating status for pvc %s", pvcKey)
 		updateStatus = false
 	}
@@ -179,6 +188,12 @@ func (ctrl *resizeController) expandAndRecover(pvc *v1.PersistentVolumeClaim, pv
 	return pvc, pv, nil, true
 }
 
+func (ctrl *resizeController) markForSlowRetry(pvcKey string, resizeStatus v1.ClaimResourceStatus) {
+	if resizeStatus == v1.PersistentVolumeClaimControllerResizeInfeasible {
+		ctrl.slowSet.Add(pvcKey)
+	}
+}
+
 func (ctrl *resizeController) callResizeOnPlugin(
 	pvc *v1.PersistentVolumeClaim,
 	pv *v1.PersistentVolume,
@@ -205,12 +220,16 @@ func (ctrl *resizeController) callResizeOnPlugin(
 					return pvc, pv, fmt.Errorf("resizing failed in controller with %v but failed to update PVC %s with: %v", err, klog.KObj(pvc), markExpansionFailedError)
 				}
 			} else {
+				// remvoe key from slowSet because controller expansion failed with non-infeasible error
+				ctrl.slowSet.Remove(pvcKey)
 				pvc, markExpansionFailedError = ctrl.markControllerExpansionFailedCondition(pvc, err)
 				if markExpansionFailedError != nil {
 					return pvc, pv, fmt.Errorf("resizing failed in controller with %v but failed to update PVC %s with: %v", err, klog.KObj(pvc), markExpansionFailedError)
 				}
 			}
 		} else {
+			// remove key from slowSet because resizer expansion failed with non-final error
+			ctrl.slowSet.Remove(pvcKey)
 			// remove key from finalErrorPVCs
 			ctrl.finalErrorPVCs.Delete(pvcKey)
 		}
@@ -218,6 +237,7 @@ func (ctrl *resizeController) callResizeOnPlugin(
 	}
 
 	ctrl.finalErrorPVCs.Delete(pvcKey)
+	ctrl.slowSet.Remove(pvcKey)
 
 	klog.V(4).InfoS("Resize volume succeeded, start to update PV's capacity", "PV", klog.KObj(pv))
 

pkg/controller/expand_and_recover_test.go
@@ -173,7 +173,7 @@ func TestExpandAndRecover(t *testing.T) {
 			controller := NewResizeController(driverName,
 				csiResizer, kubeClient,
 				time.Second, informerFactory,
-				workqueue.DefaultControllerRateLimiter(), true /*handleVolumeInUseError*/)
+				workqueue.DefaultControllerRateLimiter(), true /*handleVolumeInUseError*/, 2*time.Minute /*maxRetryInterval*/)
 
 			ctrlInstance, _ := controller.(*resizeController)
 			recorder := record.NewFakeRecorder(10)

pkg/controller/resize_status.go
@@ -39,7 +39,14 @@ func (ctrl *resizeController) markControllerResizeInProgress(
 	conditions := []v1.PersistentVolumeClaimCondition{progressCondition}
 
 	newPVC := pvc.DeepCopy()
-	newPVC.Status.Conditions = util.MergeResizeConditionsOfPVC(newPVC.Status.Conditions, conditions, false /*keepOldResizeConditions*/)
+
+	// if we are not updating status then we should preserve older conditions
+	if updateStatus {
+		newPVC.Status.Conditions = util.MergeResizeConditionsOfPVC(newPVC.Status.Conditions, conditions, false /*keepOldResizeConditions*/)
+	} else {
+		newPVC.Status.Conditions = util.MergeResizeConditionsOfPVC(newPVC.Status.Conditions, conditions, true /*keepOldResizeConditions*/)
+	}
+
 	if updateStatus {
 		newPVC = mergeStorageResourceStatus(newPVC, v1.PersistentVolumeClaimControllerResizeInProgress)
 	}

pkg/controller/resize_status_test.go
@@ -94,7 +94,9 @@ func TestResizeFunctions(t *testing.T) {
 			controller := NewResizeController(driverName,
 				csiResizer, kubeClient,
 				time.Second, informerFactory,
-				workqueue.DefaultControllerRateLimiter(), true /*handleVolumeInUseError*/)
+				workqueue.DefaultControllerRateLimiter(),
+				true, /*handleVolumeInUseError*/
+				2*time.Minute /*maxRetryInterval*/)
 
 			ctrlInstance, _ := controller.(*resizeController)
 

commit 1744e4eb4d8374f945381672572fa0cf56362d2c

util/slowset: fix data race
Reported by `go test -race`.
Also use time.Ticker to response faster to stopCh.

Fixes: 99cf66fd7606604ac3dd101b6a8141d9ff9a1366

pkg/util/slowset.go
@@ -81,18 +81,25 @@ func (s *SlowSet) TimeRemaining(key string) time.Duration {
 	return 0
 }
 
+func (s *SlowSet) removeAllExpired() {
+	s.Lock()
+	defer s.Unlock()
+	for key, t := range s.workSet {
+		if time.Since(t) > s.retentionTime {
+			delete(s.workSet, key)
+		}
+	}
+}
+
 func (s *SlowSet) Run(stopCh <-chan struct{}) {
+	ticker := time.NewTicker(s.resyncPeriod)
+	defer ticker.Stop()
 	for {
 		select {
 		case <-stopCh:
 			return
-		default:
-			time.Sleep(s.resyncPeriod)
-			for key, t := range s.workSet {
-				if time.Since(t) > s.retentionTime {
-					s.Remove(key)
-				}
-			}
+		case <-ticker.C:
+			s.removeAllExpired()
 		}
 	}
 }